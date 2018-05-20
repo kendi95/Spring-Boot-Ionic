6a4101c2ff6f19f6f1849821f7edb9bf33874c83
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-05-20 14:58:39 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/kohatsu/cursomc/servicies/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.kohatsu.cursomc.servicies;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kohatsu.cursomc.domain.Categoria;
+import com.kohatsu.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+		
+	}
+	
+}
