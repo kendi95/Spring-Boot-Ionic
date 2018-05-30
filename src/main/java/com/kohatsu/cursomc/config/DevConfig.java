8232ccc18ac9b310a16fa88bed71de7472c3bce4
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-05-30 17:53:01 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/com/kohatsu/cursomc/config/DevConfig.java
@@ -0,0 +1,38 @@
+package com.kohatsu.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.kohatsu.cursomc.servicies.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			
+			return false;
+			
+		}
+		
+		dbService.instrantiateTestDataBase();
+		
+		return true;
+		
+	}
+	
+}
