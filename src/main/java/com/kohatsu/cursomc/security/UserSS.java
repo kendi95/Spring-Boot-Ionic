5523c635b012565e0e4ddfc5331c86ca8ab8b796
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-06-01 21:53:28 -0300

    Implemenntando autenticação e geração do token JWT

---
+++ src/main/java/com/kohatsu/cursomc/security/UserSS.java
@@ -0,0 +1,88 @@
+package com.kohatsu.cursomc.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.kohatsu.cursomc.domain.enums.Perfil;
+
+public class UserSS implements UserDetails{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String email;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	
+	public UserSS() {
+		
+	}
+	
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
+		
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList());
+		
+	}
+	
+	
+	public Integer getId() {
+		
+		return this.id;
+		
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return this.authorities;
+		
+	}
+
+	@Override
+	public String getPassword() {
+		
+		return this.senha;
+		
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return this.email;
+		
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
