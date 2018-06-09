bb3788345028ad3ce9f36e033a015b2c6d786d24
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-06-09 11:23:29 -0300

    Esqueci a senha

--- src/main/java/com/kohatsu/cursomc/resources/auth/AuthResources.java
+++ src/main/java/com/kohatsu/cursomc/resources/auth/AuthResources.java
@@ -1,16 +1,22 @@
 package com.kohatsu.cursomc.resources.auth;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kohatsu.cursomc.dto.EmailDTO;
 import com.kohatsu.cursomc.security.JWTUtil;
 import com.kohatsu.cursomc.security.UserSS;
 import com.kohatsu.cursomc.servicies.UserService;
+import com.kohatsu.cursomc.servicies.auth.AuthService;
+
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/auth")
@@ -18,6 +24,8 @@ public class AuthResources {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	@Autowired
+	private AuthService authService;
 	
 	@RequestMapping(value="/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
@@ -25,6 +33,15 @@ public class AuthResources {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer "+token);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(value="/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) throws ObjectNotFoundException{
+		
+		authService.sendNewPassword(objDto.getEmail());
 		
 		return ResponseEntity.noContent().build();
 		
--- src/main/java/com/kohatsu/cursomc/servicies/email/AbstractEmailService.java
+++ src/main/java/com/kohatsu/cursomc/servicies/email/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.kohatsu.cursomc.domain.Cliente;
 import com.kohatsu.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -84,6 +85,29 @@ public abstract class AbstractEmailService implements EmailService{
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPassword) {
+		
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPassword);
+		sendEmail(sm);
+		
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPassword) {
+
+		SimpleMailMessage sm = new SimpleMailMessage();
+		
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha.");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: "+newPassword);
+		
+		return sm;
+		
+		
 	}
 	
 }
