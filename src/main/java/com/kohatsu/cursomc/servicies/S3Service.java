b7899d8bf8f03b6708c5918da0e16d8eeed2ac60
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-07-10 17:34:19 -0300

    Tratamndo exceções adequadamente

--- src/main/java/com/kohatsu/cursomc/servicies/S3Service.java
+++ src/main/java/com/kohatsu/cursomc/servicies/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.kohatsu.cursomc.servicies.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -38,7 +39,7 @@ public class S3Service {
 			
 		}catch(IOException ex) {
 			
-			throw new RuntimeException("Erro de IO: "+ex.getMessage());
+			throw new FileException("Erro de IO: "+ex.getMessage());
 			
 		}
 		
@@ -60,7 +61,7 @@ public class S3Service {
 			
 		}catch(URISyntaxException ex) {
 			
-			throw new RuntimeException("Erro ao cnonverter URL para URI");
+			throw new FileException("Erro ao cnonverter URL para URI");
 			
 		}
 		
