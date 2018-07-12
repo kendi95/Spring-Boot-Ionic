0711d1ed76608b5767e4f9cbc85803032848e8a0
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-07-12 16:18:44 -0300

    Ajustando tamanho da imagem

--- src/main/java/com/kohatsu/cursomc/servicies/ClienteService.java
+++ src/main/java/com/kohatsu/cursomc/servicies/ClienteService.java
@@ -47,6 +47,8 @@ public class ClienteService {
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	@Value("${img.profile.size}")
+	private int resize;
 	
 	
 	public Cliente find(Integer id) {
@@ -166,11 +168,15 @@ public class ClienteService {
 		}else {
 			
 			
-			BufferedImage ipgImage = imageService.getJpgImageFromFile(multipartFile);
+			BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+			
+			jpgImage = imageService.cropSquare(jpgImage);
+			
+			jpgImage = imageService.resize(jpgImage, resize);
 			
 			String fileName = prefix+user.getId()+".jpg";
 			
-			return s3Service.uploadFile(imageService.getInputStream(ipgImage, "jpg"), fileName, "image");
+			return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 			
 		}
 		
