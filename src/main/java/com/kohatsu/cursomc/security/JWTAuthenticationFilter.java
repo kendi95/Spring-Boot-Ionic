c795dacbb7b2b87efa97a01b7d5f0f66312b6d33
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-07-12 16:34:09 -0300

    Expondo o header Authorization (problema de Cors)

--- src/main/java/com/kohatsu/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/kohatsu/cursomc/security/JWTAuthenticationFilter.java
@@ -60,6 +60,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String email = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(email);
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 
 	}
 
